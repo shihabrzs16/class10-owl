c8c27a8cf92c5634af3923bcfeab1bfb4aebd2da
shihabrzs16 <75194672+shihabrzs16@users.noreply.github.com>
2020-12-10 03:08:19 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// $(document).ready(function(){
+//   $(".owl-carousel").owlCarousel();
+// });
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    mouseDrag:true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
